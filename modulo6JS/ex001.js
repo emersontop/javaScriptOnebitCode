f2b9ddf0de8214485195ca55a3fea0596512f3f5
Guilherme Lourenço <53027213+emersontop@users.noreply.github.com>
2023-02-15 10:43:56 -0300

    exercicio

---
+++ modulo6JS/ex001.js
@@ -0,0 +1,7 @@
+console.log("Bem vindo a o planeta terra");
+console.info("Bem vindo a o planeta terra");
+console.warn("Bem vindo a o planeta terra");
+console.error("Bem vindo a o planeta terra");
+
+alert("Bem vindo a o planeta terra");
+confirm("tudo ok para decolar")
